09e574f9125fd5c0b8b4ca7ba0595724d4f62f4f
Vital Lobachevskij <vlobachevsky@gmail.com>
2022-05-16 15:31:36 +0300

    Generate cert

--- config/entrypoint.sh
+++ config/entrypoint.sh
@@ -1,5 +1,5 @@
 
-if [ -f "/etc/ssl/certs/nginx.crt" ] || [ -f "/etc/ssl/nginx.pem" ]
+if [ ! -f "/etc/ssl/certs/nginx.crt" ] || [ ! -f "/etc/ssl/nginx.pem" ]
 then
 	openssl req -x509 -nodes -days 365 -subj "/C=XX/ST=XX/O=Company,Inc./CN=mydomain.com" -addext "subjectAltName=DNS:mydomain.com" -newkey rsa:2048 -keyout /etc/ssl/nginx.pem -out /etc/ssl/certs/nginx.crt;
 fi
