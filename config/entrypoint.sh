14fe71904e27764443d5b09642e2500018d1bc84
Vital Lobachevskij <vlobachevsky@gmail.com>
2022-05-16 15:50:23 +0300

    Add https

--- config/entrypoint.sh
+++ config/entrypoint.sh
@@ -4,4 +4,5 @@ then
 	openssl req -x509 -nodes -days 365 -subj "/C=XX/ST=XX/O=Company,Inc./CN=mydomain.com" -addext "subjectAltName=DNS:mydomain.com" -newkey rsa:2048 -keyout /etc/ssl/nginx.pem -out /etc/ssl/certs/nginx.crt;
 fi
 
-nginx -g 'daemon off;'; nginx -s reload;
+#nginx -g 'daemon off;'; nginx -s reload;
+nginx -t
