6360369de3ec2634481e8a9b363b26846add416d
Vital Lobachevskij <vlobachevsky@gmail.com>
2022-05-16 15:23:23 +0300

    Generate cert

--- config/entrypoint.sh
+++ config/entrypoint.sh
@@ -1,7 +1,8 @@
 
-if [ -f "/etc/ssl/certs/nginx.crt" ] || [ -f "/etc/ssl/nginx.pem" ]
-then
-	openssl req -x509 -nodes -days 365 -subj "/C=XX/ST=XX/O=Company,Inc./CN=mydomain.com" -addext "subjectAltName=DNS:mydomain.com" -newkey rsa:2048 -keyout /etc/ssl/nginx.pem -out /etc/ssl/certs/nginx.crt;
-fi
+openssl req -x509 -nodes -days 365 -subj "/C=XX/ST=XX/O=Company,Inc./CN=mydomain.com" -addext "subjectAltName=DNS:mydomain.com" -newkey rsa:2048 -keyout /etc/ssl/nginx.pem -out /etc/ssl/certs/nginx-selfsigned.crt;
+#if [ -f "/etc/ssl/certs/nginx.crt" ] || [ -f "/etc/ssl/nginx.pem" ]
+#then
+#	openssl req -x509 -nodes -days 365 -subj "/C=XX/ST=XX/O=Company,Inc./CN=mydomain.com" -addext "subjectAltName=DNS:mydomain.com" -newkey rsa:2048 -keyout /etc/ssl/nginx.pem -out /etc/ssl/certs/nginx-selfsigned.crt;
+#fi
 
 nginx -g 'daemon off;'; nginx -s reload;
