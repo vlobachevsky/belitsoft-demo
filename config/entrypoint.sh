0bb94782d0278aaef3156751352129032db2c3b7
Vital Lobachevskij <vlobachevsky@gmail.com>
2022-05-16 15:19:14 +0300

    Generate cert

--- config/entrypoint.sh
+++ config/entrypoint.sh
@@ -1,3 +1,7 @@
-echo 'In entrypoint script'
+
+if [ -f "/etc/ssl/certs/nginx.crt" ] || [ -f "/etc/ssl/nginx.pem" ]
+then
+	openssl req -x509 -nodes -days 365 -subj "/C=XX/ST=XX/O=Company,Inc./CN=mydomain.com" -addext "subjectAltName=DNS:mydomain.com" -newkey rsa:2048 -keyout /etc/ssl/nginx.pem -out /etc/ssl/certs/nginx.crt;
+fi
 
 nginx -g 'daemon off;'; nginx -s reload;
